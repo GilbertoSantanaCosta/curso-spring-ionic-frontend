b728ab1943de8c0ab6ef404b0c6605eb4cbcdda0
GilbertoSantanaCosta <you@example.comg.s.d.c@hotmail.comgit config --global user.name Your>
2021-10-25 17:11:42 -0300

    Obtendo os dado do formulario de login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { IonicPage, MenuController, NavController } from 'ionic-angular';
+import { CredenciaisDTO } from '../../models/credenciais.dto';
+
 @IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  creds : CredenciaisDTO = {
+    email : "",
+    senha : ""
+  }
 
   constructor(public navCtrl: NavController, public menu: MenuController) {
   }
@@ -20,6 +27,8 @@ export class HomePage {
 
 
     login(){
+
+      console.log(this.creds)
       this.navCtrl.setRoot('CategoriasPage')
     }
 }
